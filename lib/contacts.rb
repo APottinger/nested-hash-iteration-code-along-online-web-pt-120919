36a74c4722da120a3db9b852aedeb43ae2d349e7
Allen Phillip Pottinger <allenpottinger@gmail.com>
2019-12-21 20:11:28 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,6 +15,37 @@ contacts.each do |person, contact_details_hash|
   binding.pry
 end
 
+
+
+
+
+
+
+
+contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        binding.pry
+      end
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 contacts.each do |person, contact_details_hash|
   if person == "Freddy Mercury"
     contact_details_hash.each do |attribute, data|
